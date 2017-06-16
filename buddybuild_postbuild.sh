a4c8ad8c713dfe31ac9a18c68fc89b151ce5a6b3
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-16 17:38:31 -0400

    Tweak curl command maybe

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -6,8 +6,7 @@ cd $BUDDYBUILD_PRODUCT_DIR
 
 find . -name "*.dSYM" | xargs -I \{\} zip -r all-dSYM.zip {}
 
-curl \
-    -F "ipa=@$BUDDYBUILD_IPA_PATH" \
+curl -F "ipa=@$BUDDYBUILD_IPA_PATH" \
     -F "dsym=all-dSYM.zip" \
     -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \
     "https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload"
