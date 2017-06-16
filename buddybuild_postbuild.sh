c291e8ecfe28dcba8dd0f534d531391878d04698
Romain Pouclet <romain.pouclet@gmail.com>
2017-06-16 17:25:01 -0400

    Update to Hockey

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+printenv  | grep HOCKEYAPP
+
 cd $BUDDYBUILD_PRODUCT_DIR
 
 find . -name "*.dSYM" | xargs -I \{\} zip -r all-dSYM.zip {}
@@ -7,4 +9,4 @@ find . -name "*.dSYM" | xargs -I \{\} zip -r all-dSYM.zip {}
 curl \
     -F "ipa=@$BUDDYBUILD_IPA_PATH" \
     -F "dsym=all-dSYM.zip" \
-    -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \ https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
+    -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \ "https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload"
